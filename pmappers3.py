026fcf687e97648bdfc31e5f6258cf6ae66af470
Collins Orighose <collinsefe@yahoo.com>
2020-09-28 20:18:10 +0100

    added risk analysis

--- pmappers3.py
+++ pmappers3.py
@@ -17,6 +17,7 @@ def lambda_handler(event, context):
     bucketName = event['bucketname']
     bucketRegion = event['bucketregion']
     s3ObjectName = event['s3objectname']
+    s3ObjectName2 = event['s3objectname2']
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--profile', default='default')
@@ -34,24 +35,13 @@ def lambda_handler(event, context):
     print(filePath)
     uploaded = s3util.upload_to_s3(filePath,bucketName,s3ObjectName)
     
-    '''
-    unique_outputFile2 = "analysis_report_" + dateNow.strftime("%H-%M-%S-%f")
-    s3ObjectName2 =  'analysis_report.json'
-    filePath2 = LOCAL_STORAGE_PATH + s3ObjectName2
-    print(filePath2)
-    '''
-    
     #analysis report section
+    filePath2 = LOCAL_STORAGE_PATH + s3ObjectName2
     reportobj = find_risks.gen_report(graph_obj)
     reportdict = reportobj.as_dictionary()
-    with open('/tmp/analysis_report.json', 'w') as outfile:  
+    with open(filePath2, 'w') as outfile:  
         json.dump(reportdict, outfile)            
-    s3ObjectName2 = 'analysis_report.json'
-    filePath2 = LOCAL_STORAGE_PATH + s3ObjectName2
     print(filePath2)
-
-    #s3 upload section
-    uploaded = s3util.upload_to_s3(filePath,bucketName,s3ObjectName)
     uploaded2 = s3util.upload_to_s3(filePath2,bucketName,s3ObjectName2)
 
     return uploaded
@@ -59,15 +49,3 @@ def lambda_handler(event, context):
 
 if __name__ == '__main__':
     lambda_handler(None, None)
-
-Analysis 
-principalmapper.analysis.find_risks: 
-function gen_findings_and_print: dumps findings in markdown(text)/JSON format to stdout. Wraps around gen_report, which can be used instead for custom formatting by pulling Report and Finding objects.
-
-principalmapper.analysis.report: 
-class Report: a simple object containing metadata about generated findings (account ID, date, version of PMapper). 
-principalmapper.analysis.finding: 
-class Finding: a simple object containing data about a risk to the AWS account. 
- 
-
-
